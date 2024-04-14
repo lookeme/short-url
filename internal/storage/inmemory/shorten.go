16f5c9b5268b37bc6c92dcd4b4fcaf7c9d997ed9
Денис Белокопытов <dvabelokopytov@sberbank.ru>
2024-04-14 15:10:25 +0300

    Iteration 1

--- internal/storage/inmemory/shorten.go
+++ internal/storage/inmemory/shorten.go
@@ -3,15 +3,15 @@ package inmemory
 import "sync"
 
 type Storage struct {
-	urlToKey  map[string]string
-	keyTouUrl map[string]string
-	mutex     sync.RWMutex
+	urlToKey map[string]string
+	keyToURL map[string]string
+	mutex    sync.RWMutex
 }
 
 func NewStorage() *Storage {
 	return &Storage{
-		urlToKey:  make(map[string]string),
-		keyTouUrl: make(map[string]string),
+		urlToKey: make(map[string]string),
+		keyToURL: make(map[string]string),
 	}
 }
 
@@ -19,7 +19,7 @@ func (s *Storage) Save(key, value string) error {
 	defer s.mutex.Unlock()
 	s.mutex.Lock()
 	s.urlToKey[key] = value
-	s.keyTouUrl[value] = key
+	s.keyToURL[value] = key
 	return nil
 }
 
@@ -32,6 +32,6 @@ func (s *Storage) FindByURL(key string) (string, bool) {
 func (s *Storage) FindByKey(key string) (string, bool) {
 	defer s.mutex.RUnlock()
 	s.mutex.RLock()
-	value, ok := s.keyTouUrl[key]
+	value, ok := s.keyToURL[key]
 	return value, ok
 }
