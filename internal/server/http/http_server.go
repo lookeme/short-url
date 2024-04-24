bea7129dee57abf5ab3cd1f89d1a8b767bef5d14
Денис Белокопытов <dvabelokopytov@sberbank.ru>
2024-04-24 15:15:24 +0300

    Iteration 7

--- internal/server/http/http_server.go
+++ internal/server/http/http_server.go
@@ -19,7 +19,7 @@ func (s *Server) Serve() error {
 	r := chi.NewRouter()
 	r.Use(s.Logger.Middleware)
 	r.Post("/", s.Handler.HandlePOST)
-	r.Post("/api/shorten", s.Handler.HandlePOST)
+	r.Post("/api/shorten", s.Handler.HandleShorten)
 	r.Get("/{id}", s.Handler.HandleGet)
 	s.Logger.Log.Info("shorten url service ", zap.String("starting serving on ....", s.Config.ServerAddress))
 	return http.ListenAndServe(s.Config.ServerAddress, r)
