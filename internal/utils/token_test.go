e434b9919cffa198bfa9d545341e3794217d8468
Денис Белокопытов <dvabelokopytov@sberbank.ru>
2024-07-24 06:34:05 +0300

    iteration 17

--- internal/utils/token_test.go
+++ internal/utils/token_test.go
@@ -2,7 +2,7 @@ package utils
 
 import "testing"
 
-func BenchmarkFibo(b *testing.B) {
+func BenchmarkGetToken(b *testing.B) {
 	token := NewShortToken(20)
 	b.Run("create 20", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
